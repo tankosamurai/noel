b2af4dca76dd3815af087d423600329a820bb590
tankosamurai <tankosamurai@outlook.com>
2017-01-22 15:40:49 +0900

    First version of background.js

---
+++ background/background.js
@@ -0,0 +1,36 @@
+browser.contextMenus.create({
+  id: "download",
+  title: "Download this garally",
+  contexts: ["all"],
+  enabled: true,
+});
+
+browser.contextMenus.onClicked.addListener((info, tab) => {
+  let garellyUrl = GarellyURL.fromString(tab.url);
+  let garellyPromise = fetch("GET", garellyUrl.toString(), () => {});
+  garellyPromise.then((xhr) => {
+    let garellyExtractor = new GarellyExtractor(xhr.responseText, "text/html");
+    let pageUrls = garellyExtractor.pageAnchorHrefs().map((href) => {
+      let pageUrl = PageURL.fromString(href)
+      return pageUrl;
+    });
+
+    pageUrls.reduce((promise, current, index) => {
+      return promise.then(() => {
+        return fetch("GET", current.toString(), (xhr) => {
+          let pageExtractor = new PageExtractor(xhr.responseText, "text/html");
+          let imageUrl = ImageURL.fromString(pageExtractor.imageSrc());
+
+          browser.downloads.download({
+            url: imageUrl.toString(),
+            filename: imageFilenameTemplate("%gid%_%id%", imageUrl),
+          });
+        });
+      }, () => {
+        Promise.resolve(() => {
+          console.log("error!");
+        });
+      });
+    }, Promise.resolve());
+  });
+});
